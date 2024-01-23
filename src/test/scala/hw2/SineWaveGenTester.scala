52193f9cc120e26058101ece458569b81b976228
Scott Beamer <sbeamer@ucsc.edu>
2024-01-22 19:09:25 -0800

    modernize import for chisel 3.6

--- src/test/scala/hw2/SineWaveGenTester.scala
+++ src/test/scala/hw2/SineWaveGenTester.scala
@@ -1,7 +1,7 @@
 package hw2
 
 import chisel3._
-import chisel3.tester._
+import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 
 class SineWaveGenTester extends AnyFlatSpec with ChiselScalatestTester {
